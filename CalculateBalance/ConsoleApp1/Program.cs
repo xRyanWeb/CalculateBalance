c068a345a47445cc54992b8f86ef06ba1253ca96
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-16 19:38:12 -0500

    Added CalculateBalance

---
+++ CalculateBalance/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace App
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            for (int? i = 0; i < 1; i++)
+            {
+
+                Console.WriteLine("Enter Current Balance 1: ");
+                double price1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Current Balance 2: ");
+                double price2 = Convert.ToDouble(Console.ReadLine());
+
+                double calculation = (double)(double)(price1 - price2);
+
+                Console.Write($"Your Final Balance is: ${Double.Round(calculation, 2, MidpointRounding.AwayFromZero)}");
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
